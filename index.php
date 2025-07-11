742ba0ce88701e9229b2b46707b8ef1f6110ccad
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2025-07-11 13:17:03 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Controle de Chaves</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="logo.png" alt="Logo da Escola" width="100">
+        </div>
+        <div class="form">
+            <form action="emprestar.php" method="POST">
+                <label for="sala">Digite a sala:</label>
+                <input type="text" name="sala" required>
+                <input type="submit" value="Consultar">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
